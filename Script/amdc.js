9ef55905ac4e08e14be4ace8f0de93eeedaab0b9
清酒·ximiannai <97789607+axcsz@users.noreply.github.com>
2023-07-14 09:48:45 +0800

    Update amdc.js

--- Script/amdc.js
+++ Script/amdc.js
@@ -1,12 +1,17 @@
-// 2023-06-07 22:35
+// 2023-06-18 20:25
 
 const url = $request.url;
 const header = $request.headers;
-let ua = header["User-Agent"];
+const isQuanX = typeof $task !== "undefined";
+let ua = header["User-Agent"] || header["user-agent"];
 
 if (url.includes("/amdc/mobileDispatch")) {
   if (ua.includes("AMapiPhone") || ua.includes("Cainiao4iPhone")) {
-    $done({ status: "HTTP/1.1 404 Not Found" });
+    if (isQuanX) {
+      $done({ status: "HTTP/1.1 404 Not Found" });
+    } else {
+      $done();
+    }
   } else {
     $done({});
   }
