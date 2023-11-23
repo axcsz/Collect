b52669819b4dea118904db742a013a2b5029304d
故鸢·池鱼 <97789607+axcsz@users.noreply.github.com>
2023-11-23 20:26:10 +0800

    Delete Script directory

--- Script/JS/surgeithome.js
+++
@@ -1,7 +0,0 @@
-let i = JSON.parse($response.body);
-i.data.list = i.data.list.filter(item => {
-  return item.feedType !== 10002 //轮播
-  && item.feedType !== 10003 //置顶
-  && (item.feedType !== 10000 || (item.feedContent.smallTags[0]?.text === null || !item.feedContent.smallTags[0].text.includes("广告")));
-});
-$done({body: JSON.stringify(i)});
--- Script/yang.js
+++
@@ -1,9 +0,0 @@
-// https://github.com/yangdaozhao/ylgy/blob/master/ylgy.js
-
-let obj = JSON.parse($response.body);
-
-if (obj.blockTypeData) {
-  obj.blockTypeData = {};
-}
-
-$done({ body: JSON.stringify(obj) });
