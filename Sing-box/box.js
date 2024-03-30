557e352c4ace0da8ddb1f41ea44770ba0964bc65
清梦压星河 <97789607+axcsz@users.noreply.github.com>
2024-03-30 11:28:00 +0800

    Update box.js

--- Sing-box/box.js
+++ Sing-box/box.js
@@ -1,4 +1,4 @@
-const { type， name } = $arguments
+const { type, name } = $arguments
 const compatible_outbound = {
   tag: 'COMPATIBLE',
   type: 'direct',
@@ -8,7 +8,7 @@ let compatible
 let config = JSON.parse($files[0])
 let proxies = await produceArtifact({
   name,
-  type: /^1$|col/i.test(type) ? 'collection' : 'subscription'，
+  type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
   platform: 'sing-box',
   produceType: 'internal',
 })
@@ -16,22 +16,22 @@ let proxies = await produceArtifact({
 config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
-  if (['all'， '全部节点'].includes(i.tag)) {
+  if (['全部节点'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies))
   }
-  if (['hk'， '香港节点'].includes(i.tag)) {
+  if (['香港节点'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /港|hk|hongkong|kong kong|🇭🇰/i))
   }
-  if (['tw'， '台湾节点'].includes(i.tag)) {
+  if (['台湾节点'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /台|tw|taiwan|🇹🇼/i))
   }
-  if (['jp'， '日本节点'].includes(i.tag)) {
+  if (['日本节点'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /日本|jp|japan|🇯🇵/i))
   }
-  if (['sg'， '狮城节点'].includes(i.tag)) {
+  if (['狮城节点'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i))
   }
-  if (['us'， '美国节点'].includes(i.tag)) {
+  if (['美国节点'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|🇺🇸/i))
   }
 })
@@ -46,8 +46,8 @@ config.outbounds.forEach(outbound => {
   }
 });
 
-$content = JSON.stringify(config, null， 2)
+$content = JSON.stringify(config, null, 2)
 
-function getTags(proxies， regex) {
-  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies)。map(p => p.tag)
+function getTags(proxies, regex) {
+  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
 }
