a6b8602edae8b103f69eaf359dde5b98b2ebd19e
清梦压星河 <97789607+axcsz@users.noreply.github.com>
2024-04-10 10:31:50 +0800

    Add files via upload

--- Script/header.js
+++ Script/header.js
@@ -1,9 +1,10 @@
-// 2023-08-24 12:45
+// 2024-03-15 14:45
 
 const url = $request.url;
 const header = $request.headers;
+const headopt = header["Operation-Type"] || header["operation-type"];
+const ua = header["User-Agent"] || header["user-agent"];
 const isQuanX = typeof $task !== "undefined";
-let ua = header["User-Agent"] || header["user-agent"];
 
 if (url.includes("/amdc/mobileDispatch")) {
   if (
@@ -19,6 +20,56 @@ if (url.includes("/amdc/mobileDispatch")) {
     }
   } else {
     $done({});
+  }
+} else if (url.includes("/mobile.12306.cn/otsmobile/app/mgs/")) {
+  // 12306页面内容
+  const list12306 = [
+    // "com.cars.otsmobile.bangbangSafe.deciveInfo", // 设备序列号
+    // "com.cars.otsmobile.checkLoginStatus", // 登录信息
+    // "com.cars.otsmobile.city",
+    // "com.cars.otsmobile.initCountry",
+    // "com.cars.otsmobile.initNewSysCache",
+    // "com.cars.otsmobile.initProvince",
+    "com.cars.otsmobile.integration.activityBanner", // 活动横幅
+    "com.cars.otsmobile.memberInfo.getMemberQa", // 铁路会员 常见问题
+    // "com.cars.otsmobile.memberInfo.integrationHomeInit", // 铁路会员 会员信息
+    // "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
+    "com.cars.otsmobile.newHomePage.initData", // 热门资讯
+    "com.cars.otsmobile.newHomePageBussData" // 商品信息流
+    // "com.cars.otsmobile.newHomePageRefresh",
+    // "com.cars.otsmobile.travelPage.initData", // 出行服务
+  ];
+  if (isQuanX) {
+    if (list12306?.includes(headopt)) {
+      $done({ status: "HTTP/1.1 404 Not Found" });
+    } else {
+      $done({});
+    }
+  } else {
+    if (list12306?.includes(headopt)) {
+      $done();
+    } else {
+      $done({});
+    }
+  }
+} else if (url.includes("/mobilepaas.abchina.com.cn:441/mgw")) {
+  // 中国农业银行开屏广告
+  const listbankabc = [
+    "com.bankabc.recommendcenter.homepage.gethpadverinfo",
+    "com.abchina.mbank.common.homepage.getStartParam"
+  ];
+  if (isQuanX) {
+    if (listbankabc?.includes(headopt)) {
+      $done({ status: "HTTP/1.1 404 Not Found" });
+    } else {
+      $done({});
+    }
+  } else {
+    if (listbankabc?.includes(headopt)) {
+      $done();
+    } else {
+      $done({});
+    }
   }
 } else {
   $done({});
